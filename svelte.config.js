9818be98390e43f3ee5f0b811095136417aeb0ab
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2024-04-12 12:43:41 -0400

    chore: svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // Use adapter-node for Node.js environments
     adapter: adapter({
         pages: 'build',
         assets: 'build',
